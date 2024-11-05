12e315561f83b2001f75689c949e9929954079c2
ammarAlaliS <ammaralialiwa@gmail.com>
2024-11-05 16:52:45 -0600

    set up presentation layer and flutter native splash package

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:post_flow/presentation/pages/splash.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:post_flow/presentation/screens/get_started/get_started_screen.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Splash(),
-    );
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlowPost',
+      home: GetStartedScreen(),
+    ); 
   }
 }
-
